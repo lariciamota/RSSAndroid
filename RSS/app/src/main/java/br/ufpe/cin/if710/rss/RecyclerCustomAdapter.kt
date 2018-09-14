9e3647334fe6ea18cada7ed5a01eaa1c473c9f5b
lmmc2 <lmmc2@cin.ufpe.br>
2018-09-14 15:50:22 -0300

    Passo 7 completo

--- RSS/app/src/main/java/br/ufpe/cin/if710/rss/RecyclerCustomAdapter.kt
+++ RSS/app/src/main/java/br/ufpe/cin/if710/rss/RecyclerCustomAdapter.kt
@@ -1,5 +1,6 @@
 package br.ufpe.cin.if710.rss
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,11 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 import org.jetbrains.anko.textView
+import android.support.v4.content.ContextCompat.startActivity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+
 
 class RecyclerCustomAdapter(private val items: List<ItemRSS>) : RecyclerView.Adapter<RecyclerCustomAdapter.MyViewHolder>()  {
     class MyViewHolder(val view: LinearLayout) : RecyclerView.ViewHolder(view)
@@ -25,6 +31,17 @@ class RecyclerCustomAdapter(private val items: List<ItemRSS>) : RecyclerView.Ada
         var txtViewTitle: TextView = holder.view.getChildAt(0) as TextView
         var txtViewDate: TextView = holder.view.getChildAt(1) as TextView
         txtViewTitle.text = items[position].title
+        txtViewTitle.setOnClickListener {
+            web_page_open(items[position].link, holder.view.context)
+        }
         txtViewDate.text = items[position].pubDate
     }
+
+    fun web_page_open(urls: String, mContext : Context) {
+        val webpage = Uri.parse(urls)
+        val intents = Intent(Intent.ACTION_VIEW, webpage)
+        mContext.startActivity(intents)
+    }
+
 }
+
